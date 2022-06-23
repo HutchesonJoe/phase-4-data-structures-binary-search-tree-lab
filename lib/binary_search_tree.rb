0561870b4ad9493005ce735c59ebb06d76b70fc3
Joe <hutcheson2001@yahoo.com>
2022-06-23 18:20:41 -0400

    commit tree 9988fc103bae45574cf9ddaa37cee8daa2e444bc

--- lib/binary_search_tree.rb
+++ lib/binary_search_tree.rb
@@ -1,14 +1,33 @@
 require_relative './node'
+require "pry"
 
 class BinarySearchTree
-  attr_accessor :root
+  attr_accessor :root, :left, :right
 
-  def initialize(root = nil)
+  def initialize(root = nil, left = nil, right = nil)
     @root = root
+    @left = left
+    @right = right
   end
 
-  def search(value)
-    # your code here
+  def search(x)
+    result = []
+    nodes_to_visit = [@root]
+    # binding.pry
+    while nodes_to_visit.length > 0
+      node = nodes_to_visit.shift
+    #  binding.pry
+      if node && node.value == x
+        return node
+      elsif node && node.left.value == x
+        return node.left
+      elsif node && node.right.value == x
+        return node.right
+      end
+      result.push(node)
+    end
+    return
+    
   end
 
   def insert(value)
